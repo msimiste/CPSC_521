7516cd7726c84fca18fb5114bb639ceab2ba5983
Mike Simister <mikesimister@shaw.ca>
2017-09-19 18:14:49 -0600

    relgrp partially complete

--- Assign1.hs
+++ Assign1.hs
@@ -43,17 +43,16 @@ keepOdds c (x:xs) = if c `mod` 2 == 1 then x:(keepOdds (c+1) xs) else keepOdds (
 
 --6. Write a function to merge two lists of integers, taking the least first element at each step: mergeInt :: ([Integer],[Integer]) -> [Integer] 
 mergeInt:: ([Integer],[Integer]) -> [Integer]
-mergeInt ([],[]) = []
-mergeInt(xs,[]) = xs
 mergeInt ([],xs) = xs
+mergeInt (xs,[]) = xs 
 mergeInt((x:xs),(y:ys)) 
-    | x < y = x:mergeInt(xs,(y:ys))
+    | x <= y = x:mergeInt(xs,(y:ys))
     | otherwise = y:mergeInt((x:xs),ys)
 
 --7. Write a mergesort for integers:  mergesortInt :: [Integer] -> [Integer].     
 mergesortInt:: [Integer] -> [Integer]
 mergesortInt [] = []
-mergesortInt [x,y] = [min x y, max x y ]
+mergesortInt [x] = [x]
 mergesortInt xs = mergeInt (mergesortInt a, mergesortInt b)
     where  
         (a,b) = splitAt n xs 
@@ -61,16 +60,15 @@ mergesortInt xs = mergeInt (mergesortInt a, mergesortInt b)
     
 --8. Generalize the mergesort to arbitrary ordered type (using the class system).
 mergeGen:: (Ord a) => ([a],[a]) -> [a]
-mergeGen ([],[]) = []
 mergeGen(xs,[]) = xs
 mergeGen ([],xs) = xs
 mergeGen((x:xs),(y:ys)) 
-    | x < y = x:mergeGen(xs,(y:ys))
+    | x <= y = x:mergeGen(xs,(y:ys))
     | otherwise = y:mergeGen((x:xs),ys)
     
 merge:: (Ord a) => [a] -> [a]
 merge [] = []
-merge [x,y] = mergeGen([x],[y])
+merge [x] = [x]
 merge xs = mergeGen (merge a, merge b)
     where  
         (a,b) = splitAt n xs 
@@ -85,11 +83,10 @@ merge xs = mergeGen (merge a, merge b)
 --For example if the relation is the divide relation then 
 --relgrp div [2,3] [1,2,3,4,5,6] = [(2,[2,4,6]),(3,[3,6])]].
 
---relgrp:: (a -> b -> Bool) -> [a] -> [b] -> [(a,[b])]
---relgrp f [] bs = []
---relgrp f as [] = []
---relgrp f (a:as) (b:bs) = (a,(filter (f a bs))):(relgrp f as bs)
-
+relgrp:: (a -> b -> Bool) -> [a] -> [b] -> [(a,[b])]
+relgrp f [] bs = []
+relgrp f as [] = []
+relgrp f (a:as) (b:bs) = (a, filter (f a) (b:bs)):(relgrp f as (b:bs))
 
 
 --12. Program the "group" function: given a predicate pred:: a -> a -> Bool and a list the group function breaks the list into a series of (maximal) sublists such that any two consecutive elements satisfy the predicate pred.   The type of the function group is group:: (a -> a -> Bool) -> [a] -> [[a]].  An example of its use is as follows: suppose that the predicate nbr determines whether the absolute difference of two integers is at most 1 (i.e. they are equal or they differ by one) then group nbr [2,1,3,4,5,5,4,7,4,3,3] = [[2,1],[3,4,5,5,4],[7],[4,3,3]] : program up this example to make sure your group works.  What is group nbr []?
